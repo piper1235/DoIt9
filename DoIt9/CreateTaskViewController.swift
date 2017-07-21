d5ab589865cbc6540b1a1b9a802cf6b2f8e0a13f
Tony <piper1235@yahoo.com>
2017-07-21 13:20:54 -0700

    Added CreateTaskViewController with buttons and text field.

---
+++ DoIt9/CreateTaskViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt9
+//
+//  Created by Tony on 7/21/17.
+//  Copyright © 2017 Tony. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var TaskNameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+}
--- DoIt9/ViewController.swift
+++ DoIt9/ViewController.swift
@@ -57,5 +57,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
 }
 
