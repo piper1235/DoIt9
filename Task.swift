d3f68e0ba5084238c74ea0f8642277d8cbdc2d05
Tony <piper1235@yahoo.com>
2017-07-21 12:33:35 -0700

    initial commit

---
+++ Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt9
+//
+//  Created by Tony on 7/21/17.
+//  Copyright © 2017 Tony. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
